b3febbbd6e23afa2e233a4aca960edeffd999182
Tanya Bihag <tbihag@gmail.com>
2019-08-15 04:29:46 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
 def countdown
+  number = 10
+   while number < 10
+    puts "#{number} SECONDS"
+    number -= 1 
+    
+end
+
+def countdown_with_sleep
+  number = 10
+  sleep(1.second) while number < 10
+    puts "#{number} SECONDS"
+    number -= 1 
+    
 end
